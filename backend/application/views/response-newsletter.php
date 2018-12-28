a05804786c1c4331f4df02c5867e6fd3524f0bd3
innovasistemas <innovasistemas@gmailcom>
2018-12-28 14:41:09 -0500

    Ajuste favicon respuesta newsletter

--- backend/application/views/response-newsletter.php
+++ backend/application/views/response-newsletter.php
@@ -10,7 +10,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
-        <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
+        <link rel="shortcut icon" type="image/x-icon" href="http://medellinenlacabeza.medellinjoven.com/public/assets/images/favicon.ico" />
         
     </head>
     
